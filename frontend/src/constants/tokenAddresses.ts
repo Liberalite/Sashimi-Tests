99839a33f13597b21ff7dd084795faf520f5e3a7
Liberalite <liberalite@gmail.com>
2020-09-29 14:33:56 +0300

    Frontend Added !

---
+++ frontend/src/constants/tokenAddresses.ts
@@ -0,0 +1,4 @@
+export const yam = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
+export const yamv2 = '0xaba8cac6866b83ae4eec97dd07ed254282f6ad8a'
+export const sushiAddress = '0x48eb4C532a0728728b9ce00f10FBE583C262Fe67'
+export const masterChefAddress = '0xeB494dF7aae51dFF80562fADAAb8fa0F1Ac34910'
